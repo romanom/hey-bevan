601abd8664d0f663a052e48bf2fc70dad3481c01
Derek Kim <wkim505@aucklanduni.ac.nz>
2019-09-05 16:37:37 +1200

    profile changes

--- frontend/src/components/profile.js
+++ frontend/src/components/profile.js
@@ -4,21 +4,25 @@ import "./styles/profile.css";
 
 class Profile extends Component {
   state = {
-    userName : "",
+    userName: "",
     userImage: null,
-    totalBevans : 0
-  }
-  componentDidMount(){
+    totalBevans: 0
+  };
+  componentDidMount() {
     const response = serviceFunc.getUserRedeemableTotal(1);
-    this.setState({ userName : response.userName , userImage: response.userImage , totalBevans : response.totalBevans})
+    this.setState({
+      userName: response.userName,
+      userImage: response.userImage,
+      totalBevans: response.totalBevans
+    });
   }
   render() {
     return (
-      <div>
-        <p><img src={"this.state.userImage"}></img></p>
+      <div class="profile-container">
+        <img src={"this.state.userImage"}></img>
         <p>{this.state.userName}</p>
-        <p>Redeemable HeyBevans</p>
-        <p>{this.state.totalBevans}</p>
+        <p class="redeemable-text">Redeemable HeyBevans</p>
+        <p class="no-padding">{this.state.totalBevans}</p>
       </div>
     );
   }
