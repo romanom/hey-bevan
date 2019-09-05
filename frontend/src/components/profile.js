3430bc35623fcb111d2c3956976758d20b558e80
Derek Kim <wkim505@aucklanduni.ac.nz>
2019-09-05 16:17:30 +1200

    images + profile changes

--- frontend/src/components/profile.js
+++ frontend/src/components/profile.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
+import Jp from "./images/jp.png";
+import "./styles/profile.css";
 class Profile extends Component {
   render() {
     return (
       <div>
-        <p>Image</p>
+        <img class="profile-pic" src={Jp}></img>
         <p>{this.props.userName}</p>
-        <p>Redeemable Hey-Bevans</p>
+        <p>Redeemable HeyBevans</p>
         <p>{this.props.total}</p>
       </div>
     );
