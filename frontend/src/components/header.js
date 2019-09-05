2ffdc6b5a5baff9066dc7675e47c20a698ae8cf8
sinusudhakaran <sinu.sudhakaran@myob.com>
2019-09-06 10:38:45 +1200

    Logo click goes to home page

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -3,6 +3,7 @@ import Menu from "./menu";
 import Logo from "./images/logo_name.png";
 import "./styles/header.css";
 import SlackLogin from "./slacklogin";
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
   state = {};
@@ -10,7 +11,7 @@ class Header extends Component {
     return (
       <div class="myob-gradient white-text" id="header-main">
         <div id="header-container">
-          <img id="logo" alt="logo" src={Logo} />
+          <Link to="/"><img id="logo" alt="logo" src={Logo} /></Link>
           <div id="menu-container">
             <Menu/>
           </div>
