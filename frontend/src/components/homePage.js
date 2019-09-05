c726dcc9aa800f83869728aca3cebb2c0bb78bad
Monique Warrington <niquewarrington@gmail.com>
2019-09-06 09:46:03 +1200

    progress on homePage

--- frontend/src/components/homePage.js
+++ frontend/src/components/homePage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from 'header';
 import Footer from 'footer';
+import Bevan from "./images/logo.png";
 
 
 class HomePage extends Component {
@@ -11,6 +12,13 @@ class HomePage extends Component {
         <div classname="Homepage-header">
           <Header />
         </div>
+        <div className="BevansHead">
+          <img
+            id="largeBevansHead"
+            alt="Bevans head"
+            src={Bevan}></img>
+        </div>
+        <div className="ReasonsWhyPoster"></div>
         <Footer />
       </div>
     );
