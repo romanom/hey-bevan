3ab0831c114668cfdb64d874d0c8290e232bda6a
Derek Kim <wkim505@aucklanduni.ac.nz>
2019-09-06 10:49:47 +1200

    ?

--- frontend/src/components/menu.js
+++ frontend/src/components/menu.js
@@ -1,14 +1,20 @@
 import React, { Component } from "react";
 import "./styles/menu.css";
-import { Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 class Menu extends Component {
   render() {
     return (
       <ul id="menu">
-        <li><Link to="/leaderboard">Leaderboard</Link></li>
-        <li><Link to="/activity">Activity</Link></li>
-        <li><Link to="/tags">Tags</Link></li>
+        <li>
+          <Link to="/leaderboard">Leaderboard</Link>
+        </li>
+        <li>
+          <Link to="/activity">Activity</Link>
+        </li>
+        <li>
+          <Link to="/tags">Tags</Link>
+        </li>
       </ul>
     );
   }
