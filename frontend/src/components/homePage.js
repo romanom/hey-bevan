eba80716da6bcca6245e2f709d6840f47a158ab2
Monique Warrington <niquewarrington@gmail.com>
2019-09-06 09:54:48 +1200

    Added Bevans Revards and Bevans Reasons poster

--- frontend/src/components/homePage.js
+++ frontend/src/components/homePage.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Header from 'header';
 import Footer from 'footer';
 import Bevan from "./images/logo.png";
+import ReasonsWhyImage from "./images/BevansReasons.png";
+import Rewards from "./images/BevansRewards.png";
 
 
 class HomePage extends Component {
@@ -18,7 +20,18 @@ class HomePage extends Component {
             alt="Bevans head"
             src={Bevan}></img>
         </div>
-        <div className="ReasonsWhyPoster"></div>
+        <div className="ReasonsWhyPoster">
+          <img id="reasonsWhyImage"
+            alt="Reasons Why you should give a Bevan!"
+            src={ReasonsWhyImage}>
+          </img>
+        </div>
+        <div className="RewardsPoster">
+          <img id="rewardsPoster"
+            alt="Rewards you can get from redeeming your Bevans"
+            src={Rewards}>
+          </img>
+        </div>
         <Footer />
       </div>
     );
