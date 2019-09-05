a6b478f83f0d6575fbed2b7f9bcc9fae22e58000
sinusudhakaran <sinu.sudhakaran@myob.com>
2019-09-06 08:30:48 +1200

    Changed table style and scripts and config

--- frontend/src/components/slacklogin.js
+++ frontend/src/components/slacklogin.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import configurations from "./../config.json";
+//import { WebClient } from '@slack/web-api';
 
 export default class SlackLogin extends Component{
     state = {
@@ -9,11 +10,12 @@ export default class SlackLogin extends Component{
     render() {
         console.log("Slack Login");
         console.log('Logged in user ', this.state.loggedInUser);
+        //const token = "xoxb-2315277109-749184996816-mTCSqqL485bJl3SLSmEeVVL5";
         return (
             <div>
                 {
                     (this.state.loggedInUser === null) ?
-                    <a id="slacklink" href={`https://slack.com/oauth/authorize?scope=identity.basic&client_id=${configurations.slackClientId}&redirect_uri=localhost:3000`}><img id="slackimage" src="https://api.slack.com/img/sign_in_with_slack.png" /></a> 
+                    <a id="slacklink" href={`https://slack.com/oauth/authorize?scope=identity.basic&client_id=${configurations.slackClientId}&redirect_uri=http://localhost:3000`}><img id="slackimage" src="https://api.slack.com/img/sign_in_with_slack.png" /></a> 
                     : <div><p>user image</p><p>{this.state.loggedInUser}</p> </div>
                 }
             </div>
