dc75b4673f3dc60ffffb40d691b113f9c0833e97
Monique Warrington <niquewarrington@gmail.com>
2019-09-06 11:39:22 +1200

    First cards implemented on home

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Bevan from "./images/logo.png";
+import Achievement from "./images/reason-achievement.png";
+import Easy from "./images/reason-easy.png"
 import ReasonsWhyImage from "./images/BevansReasons.png";
 import Rewards from "./images/BevanRewards.png";
 import "./styles/home.css";
@@ -17,6 +19,22 @@ export default class Home extends Component {
                         <h1 id="bevan-tagline">
                             Too often we get caught up in our work and forget to recognize each other, have a little fun, and celebrate. HeyBevan! is the answer.
                         </h1>
+                    </div>
+                </div>
+                <div className="flex-container">
+                    <div className="reason-card">
+                        <img src={Achievement} alt="Achievment Icon" />
+                        <div class="container">
+                            <h4><b>Celebrate An Achievement</b></h4>
+                            <p>Boost morale by recognising your team's achievements and effort</p>
+                        </div>
+                    </div>
+                    <div className="reason-card">
+                        <img src={Easy} alt="Easy Icon" />
+                        <div class="container">
+                            <h4><b>Easy to Use</b></h4>
+                            <p>It only takes a few seconds to tag your team and give a Bevan</p>
+                        </div>
                     </div>
                 </div>
                 <div className="ReasonsWhyPoster">
