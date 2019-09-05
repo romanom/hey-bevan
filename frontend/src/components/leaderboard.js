cfddeb53ac40f844977f6e6b5021d16a8f2671db
Derek Kim <wkim505@aucklanduni.ac.nz>
2019-09-05 14:31:53 +1200

    resolved merge

--- frontend/src/components/leaderboard.js
+++ frontend/src/components/leaderboard.js
@@ -1,32 +1,39 @@
 import React, { Component } from "react";
 import getLeaderboardData from "./../service/service";
-
+import "./styles/leaderboard.css";
 class Leaderboard extends Component {
   state = {
     leaderboardData: []
-  }
+  };
 
-  componentDidMount(){
-    this.setState({ leaderboardData : getLeaderboardData(this.props.type, this.props.dateType, this.props.channel ) });
+  componentDidMount() {
+    this.setState({
+      leaderboardData: getLeaderboardData(
+        this.props.type,
+        this.props.dateType,
+        this.props.channel
+      )
+    });
   }
 
   render() {
     return (
       <div>
-        <table>
+        <table id="leaderboard" border="1">
           <tr>
             <th>Rank</th>
             <th>Person</th>
             <th>Total Hey-Bevans</th>
           </tr>
-          { this.state.leaderboardData.map(leaderboard => 
-          <tr>
-            <td>{leaderboard.rank}</td>
-            <td>{leaderboard.name} {leaderboard.image}</td>
-            <td>{leaderboard.total}</td>
-          </tr>
-          )
-          }
+          {this.state.leaderboardData.map(leaderboard => (
+            <tr>
+              <td>{leaderboard.rank}</td>
+              <td>
+                {leaderboard.name} {leaderboard.image}
+              </td>
+              <td>{leaderboard.total}</td>
+            </tr>
+          ))}
         </table>
       </div>
     );
