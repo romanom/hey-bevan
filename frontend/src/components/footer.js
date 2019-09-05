a51a8b0a2976279bdcfb3448c1a9aeaaf8d4acfe
Ivan Perevernykhata <ivan.perevernykhata@myob.com>
2019-09-06 10:49:20 +1200

    - class to className

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -5,8 +5,8 @@ import "./styles/footer.css";
 class Footer extends Component {
   render() {
     return (
-      <div class="myob-gradient" id="footer">
-        <img alt="myoblogo" class="myob-logo" src={MYOB}></img>
+      <div className="myob-gradient" id="footer">
+        <img alt="myoblogo" className="myob-logo" src={MYOB}></img>
         <ul className="footer-menu">
           <li>
             <a>How it works</a>
