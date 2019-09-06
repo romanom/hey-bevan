abf4d13aa56f58c9ad50dfc9a8d0b590e8b8fc82
sinusudhakaran <sinu.sudhakaran@myob.com>
2019-09-06 13:13:33 +1200

    Slack login link is changed

--- frontend/src/components/signincallback.js
+++ frontend/src/components/signincallback.js
@@ -1,11 +1,17 @@
 import React ,  {Component} from 'react';
-import QueryString from 'query-string';
+import Configurations from './../config.json';
+//import { WebClient } from '@slack/web-api';
 
 export default class SignInCallback extends Component {
     render () {
         console.log(this.props.history.location.search);
-        const params = QueryString.parse(this.props.history.location.search);
-        console.log(params);
+        console.log(Configurations.slackClientId);
+        //const client = new WebClient(Configurations.slackClientId);
+        //client.api.
+
+        const params = new URLSearchParams(this.props.history.location.search);
+        const token = params.token;
+        console.log(params, token);
         return (
             <div />
         );
